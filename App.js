d1cdb21dd343259815cb21c4863b7eaa70e0b9b6
Prashanth14 <kadireprashanthreddy1@gmail.com>
2023-12-02 21:19:56 -0500

    ignite our react app

--- App.js
+++ App.js
@@ -1,19 +1,5 @@
-/*
-    <div id = 'parent'>
-        <div id = 'child'>
-            <h1>I'm h1 tag</h1>
-            <h2>I'm h2 tag</h2>
-        </div>
-    </div>
-    <div id = 'parent'>
-        <div id = 'child2'>
-            <h1>I'm h1 tag</h1>
-            <h2>I'm h2 tag</h2>
-        </div>
-    </div>
-
-    React.createElement() returns an object while rendering react converts javascript object to HTML(Browser Understands)
-*/
+import React from "react";
+import ReactDOM from "react-dom/client"
 
 const heading = React.createElement(
    "div",
@@ -23,7 +9,7 @@ const heading = React.createElement(
     {id:"child"},
     [React.createElement(
         "h1",
-        {}, 
+        {},
         "I am h1 tag"),
     React.createElement(
         "h2",

141948e363a02d04b234b9258d4f0b71dbdbfdb0
Prashanth14 <kadireprashanthreddy1@gmail.com>
2023-12-05 15:31:37 -0500

    laying the foundation

--- App.js
+++ App.js
@@ -1,38 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement(
-   "div",
-   {id:"parent"},
-  [ React.createElement(
-    "div",
-    {id:"child"},
-    [React.createElement(
-        "h1",
-        {},
-        "I am h1 tag"),
-    React.createElement(
-        "h2",
-        {}, 
-        "I am h2 tag")
-    ]
-    ),
-    React.createElement(
-        "div",
-        {id:"child2"},
-        [React.createElement(
-            "h1",
-            {}, 
-            "I am h1 tag"),
-        React.createElement(
-            "h2",
-            {}, 
-            "I am h2 tag")
-        ]
-        )
-    ]
+//create react element using core react, React.creatElement() => object => HTML object(after rendering)
+// const heading = React.createElement(
+//     "h1",
+//     {id:"heading"},
+//     "This is React"
+//     );
+
+// console.log(heading);
+
+const elem = <span>React element </span>
+
+//create react element using JSX
+const title = (
+    <h1 className="head" tabIndex="5">
+    {elem}
+    React using JSX
+    </h1>
 );
 
+//react component
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+    React using JSX
+    </h1>
+);
+
+
+//React Functional Component, this is called component composition
+const HeadingComponent = () =>(
+    <div id="container">
+        <Title /> 
+        <h1 className="heading">React Functional Component</h1>
+    </div>
+);
+
+
+
+//create a root element for DOM(browser to display)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-    
-root.render(heading);
+
+
+//rendering react element but not react components
+// root.render(title);
+
+//rendering react component
+root.render(<HeadingComponent/>);
+
+
